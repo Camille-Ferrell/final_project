953e59ac7450783ca33458506d8063f0b421dc19
Camille-Ferrell <ferrell.c@northeastern.edu>
2023-11-28 18:19:52 -0500

    Alright so I individually tested the Music Library class and she works
    I also edited the Weather Analyzer class to have the song logic
    based on BPM
    
    Changed the Song class but now there's no inhertence so I gotta think that up

--- finalproject.py
+++ finalproject.py
@@ -3,119 +3,116 @@ from datetime import datetime
 import librosa
 import csv
 
-class Song:
-    def __init__(self, title, artist, bpm):
-        self.title = title
-        self.artist = artist
-        self.bpm = bpm
-
-class UpbeatSong(Song):
-    def __init__(self, title, artist, bpm):
-        super().__init__(title, artist, bpm)
-
-class SlowSong(Song):
-    def __init__(self, title, artist, bpm):
-        super().__init__(title, artist, bpm)
-
-class HolidaySong(Song):
-    def __init__(self, title, artist, bpm):
-        super().__init__(title, artist, bpm)
-
-def get_weather_data(api_key: str = "4204310b04c096285fad8d4b5df1c49d", city: str = "Boston"):
-    base_url = "http://api.openweathermap.org/data/2.5/weather?q=boston,ma"
-    params = {"q": city, "APPID": api_key}
-
-    try:
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()  # Raise an exception for bad requests
-
-        # Assuming the API response contains JSON data
-        data = response.json()
-
-        # Extracting relevant weather information (replace with actual data structure from the API)
-        temperature = data["main"]["temp"]
-        condition = data["weather"][0]["description"]
-
-        return f"The weather in {city} is {condition} with a temperature of {temperature}°C."
-
-    except requests.exceptions.RequestException as e:
-        return f"Error fetching weather data: {e}"
-
-def get_calendar_date():
-    return datetime.now().strftime("%Y-%m-%d")
-
-def analyze_song_bpm(song_path):
-    # Use Librosa to analyze the BPM of the song
-    y, sr = librosa.load(song_path)
-    onset_env = librosa.onset.onset_strength(y, sr=sr)
-    bpm = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)[0]
-    return bpm
-
 class MusicLibrary:
-    def __init__(self):
-        self.songs = []
+    def __init__(self, songs):
+        self.songs = songs
 
-    def load_library_from_csv(self, file_path):
+    @classmethod
+    def load_library_from_csv(cls,file_path):
+        library = []
         try:
-            with open(file_path, 'r') as csv_file:
+            with open(file_path,'r') as csv_file:
                 csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
+                for column in csv_reader:
                     song = Song(
-                        title=row['song_name'],
-                        artist=row['artist'],
-                        bpm=int(row['bpm'])
+                        title=column['Song'],
+                        artist=column[' Artist(s)'],
+                        bpm=int(column[' BPM']),
+                        holiday_association=column[' Holiday Association']
                     )
-                    self.songs.append(song)
+                    library.append(song)
             print(f"Library loaded from {file_path}")
         except FileNotFoundError:
             print(f"File not found: {file_path}")
         except Exception as e:
             print(f"Error loading library: {e}")
-
-    def analyze_song(self, song_path):
-        # Use Librosa to analyze the BPM of the song
-        bpm = analyze_song_bpm(song_path)
-        return bpm
+        return cls(library)
 
     def display_library(self):
         for song in self.songs:
             print(f"{song.title} by {song.artist} ({song.bpm} BPM)")
 
+class Song:
+    def __init__(self, title, artist, bpm, holiday_association=None):
+        self.title = title
+        self.artist = artist
+        self.bpm = bpm
+        self.holiday_association = holiday_association
+
 class WeatherAnalyzer:
-    def __init__(self, weather_conditions):
-        self.weather_conditions = weather_conditions
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_user_input(self):
+        city = input("Enter the city for weather analysis: ")
+        return city
+    
+    def get_weather_data(self, city):
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {"q": city, "APPID": self.api_key}
+
+        try:
+            response = requests.get(base_url, params=params)
+            response.raise_for_status()  # Raise an exception for bad requests
+
+            # Assuming the API response contains JSON data
+            data = response.json()
+
+            # Extracting relevant weather information (replace with actual data structure from the API)
+            temperature = data["main"]["temp"]
+            condition = data["weather"][0]["description"]
+
+            return f"The weather in {self.city} is {condition} with a temperature of {temperature}°C."
+
+        except requests.exceptions.RequestException as e:
+            return f"Error fetching weather data: {e}"
 
     def analyze_weather(self):
-        if "clear" in self.weather_conditions.lower():
-            return UpbeatSong("Sunny Song", "Artist", 120)
-        elif "cloud" in self.weather_conditions.lower():
-            return SlowSong("Cloudy Song", "Artist", 80)
+        city = self.get_user_input()
+        weather_data = self.get_weather_data(city)
+        # Replace this with your logic based on weather condition
+        # Extracting relevant weather information
+        temperature = int(weather_data.split('°')[0].split(':')[-1])
+        condition = weather_data.split('is ')[1].split(' with')[0].lower()
+
+        #My logic based on weather conditions
+        if "clear" in condition or "sunny" in condition:
+            if temperature > 60:
+                return Song("Sunny and Warm Song", "Artist", 120)
+            else:
+                return Song("Sunny and Cool Song", "Artist", 110)
+        elif "partly cloudy" in condition or "partly sunny" in condition:
+            return Song("Partly Cloudy Song", "Artist", 100)
+        elif "cloudy" in condition or "rainy" in condition or "foggy" in condition or "snowy" in condition:
+            return Song("Rainy Day Song", "Artist", 89)
         else:
-            return UpbeatSong("Default Song", "Artist", 100)
+            # Default case, return a default song
+            return Song("Default Song", "Artist", 100)
 
 class CalendarAnalyzer:
     def __init__(self, calendar_date):
         self.calendar_date = calendar_date
 
+    @staticmethod
+    def get_calendar_date():
+        return datetime.now().strftime("%Y-%m-%d")
+
     def analyze_date(self):
         # Check for specific holidays and return a HolidaySong
         if "12-25" in self.calendar_date:
-            return HolidaySong("Christmas Song", "Artist", 90)
+            return Song("Christmas Song", "Artist", 90)
         elif "10-31" in self.calendar_date:
-            return HolidaySong("Halloween Song", "Artist", 100)
+            return Song("Halloween Song", "Artist", 100)
         else:
             return None
 
 class SongRecommendation:
-    def __init__(self, library, weather_conditions, calendar_date):
-        self.music_library = MusicLibrary(library)
-        self.weather_analyzer = WeatherAnalyzer(weather_conditions)
-        self.calendar_analyzer = CalendarAnalyzer(calendar_date)
+    def __init__(self, library, weather_analyzer, calendar_analyzer):
+        self.music_library = library
+        self.weather_analyzer = weather_analyzer
+        self.calendar_analyzer = calendar_analyzer
 
-    def recommend_song(self, song_path):
-        # Analyze the song's BPM
-        bpm = self.music_library.analyze_song(song_path)
-
+    def recommend_song(self):
         # Analyze weather conditions
         weather_song = self.weather_analyzer.analyze_weather()
 
@@ -125,17 +122,18 @@ class SongRecommendation:
         # Choose the final recommended song based on conditions
         if holiday_song:
             return holiday_song
-        elif bpm > 100:
-            return UpbeatSong("High BPM Song", "Artist", bpm)
         else:
-            return SlowSong("Low BPM Song", "Artist", bpm)
+            return weather_song
 
-# Example Usage
+
+                       
+#Example Usage
 api_key = "4204310b04c096285fad8d4b5df1c49d"
-city = "Boston"
-weather_data = get_weather_data(api_key, city)
-calendar_date = get_calendar_date()
-library = ["path/to/song1.mp3", "path/to/song2.mp3", "path/to/song3.mp3"]
-song_recommendation = SongRecommendation(library, weather_data, calendar_date)
-recommended_song = song_recommendation.recommend_song("path/to/user_selected_song.mp3")
+file_path = "Music_Library.csv"
+weather_analyzer = WeatherAnalyzer(api_key)
+calendar_date = CalendarAnalyzer.get_calendar_date()
+library = MusicLibrary.load_library_from_csv(file_path)
+song_recommendation = SongRecommendation(library, weather_analyzer, CalendarAnalyzer(calendar_date))
+recommended_song = song_recommendation.recommend_song()
 print(f"Recommended Song: {recommended_song.title} by {recommended_song.artist} with BPM: {recommended_song.bpm}")
+
